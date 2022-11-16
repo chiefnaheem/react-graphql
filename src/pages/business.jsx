a6d012b16a511eddf6950c1a7e0378961d96ac6b
naheem adedokun <nadedokun@heckerbella.com>
2022-11-16 21:38:31 +0100

    business page

---
+++ src/pages/business.jsx
@@ -0,0 +1,38 @@
+import { Link, useParams } from 'react-router-dom';
+import Spinner from '../components/spinner';
+import DeleteBusinessButton from '../components/deleteBusiness';
+import EditBusinessForm from '../components/editBusiness';
+import { useQuery } from '@apollo/client';
+import { GET_BUSINESS } from '../queries/businessQueries';
+
+export default function BusinessPage() {
+  const { id } = useParams();
+  const { loading, error, data } = useQuery(GET_BUSINESS, { variables: { id } });
+
+  if (loading) return <Spinner />;
+  if (error) return <p>Something Went Wrong</p>;
+
+  return (
+    <>
+      {!loading && !error && (
+        <div className='mx-auto w-75 card p-5'>
+          <Link to='/' className='btn btn-light btn-sm w-25 d-inline ms-auto'>
+            Back
+          </Link>
+
+          <h1>{data.business.name}</h1>
+          <p>{data.business.type}</p>
+         
+          <p >{data.business.address}</p>
+
+
+
+          <EditBusinessForm project={data.business} />
+
+          <DeleteBusinessButton projectId={data.business.id} />
+        </div>
+      )}
+    </>
+  );
+}
+Footer
